428a1e70a104544f21f5b9b14f9c51b01def9168
insvk <xxxtentactionff23@gmail.com>
2026-08-05 20:19:45 +0530

    Fix case sensitivity in SystemLog filename

--- src/main/java/com/quizapp/model/Systemlog.java
+++ src/main/java/com/quizapp/model/Systemlog.java
@@ -1,7 +1,12 @@
 package com.quizapp.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "system_logs")
